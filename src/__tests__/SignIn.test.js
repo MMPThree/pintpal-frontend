f9939f11ac93e96b7f7170f4471539a06d593291
peter ji <hyeongjun9@gmail.com>
2023-09-21 15:40:15 -0700

    signin signup done

---
+++ src/__tests__/SignIn.test.js
@@ -0,0 +1,25 @@
+import {render, screen} from "@testing-library/react"
+import SignIn from "../pages/SignIn"
+import { BrowserRouter } from "react-router-dom"
+
+describe("<SignIn />", () => {
+    it("renders without crashing", () => {
+        render(
+            <BrowserRouter>
+            <SignIn />
+            </BrowserRouter>
+        )
+        const indexLink = screen.getByRole('heading', {
+            name: /login/i
+          })
+        expect(indexLink).toBeInTheDocument()
+        const inputForm = screen.getByText(/email: password:/i)
+        expect(inputForm).toBeInTheDocument()
+        const button = screen.getByRole('button', {
+            name: /login/i
+          })
+        expect(button).toBeInTheDocument()
+        const navlink = screen.getByText(/not registered\?/i)
+        expect(navlink).toBeInTheDocument()
+    })
+})
